eb37d94e87ba1e15e8f74d5dd7a8ac229b3608cb
Pavel Borissiouk <pashaborisyk@gmail.com>
2020-09-27 20:01:17 +0300

    build artifact

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -37,9 +37,8 @@ export class HomeComponent implements OnInit {
               this.categories.push(menu.positionGroup);
             }
           });
-          this.categories = this.categories.reverse()
           this.menu = result;
-          const activeMenu: Menu = result.length > 0 ? result[result.length - 1] : null;
+          const activeMenu: Menu = result.length > 0 ? result[0] : null;
           if (activeMenu) {
             this.activePositions = activeMenu.positions;
             this.categoryService.changeActiveCategory(activeMenu.positionGroup);
